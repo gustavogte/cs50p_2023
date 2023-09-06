2b8a186132c0cb0261334b467b9872713d0d30bb
gustavogte <gustavogte@gmail.com>
2023-09-05 21:13:21 -0600

    new student_class4 with @classmethod

---
+++ w8_OOP/student_class4.py
@@ -0,0 +1,46 @@
+class Student:
+    def __init__(self, name, house):
+        self.name = name
+        self.house = house
+
+    def __str__(self):
+        return f"{self.name} from {self.house}"
+
+    @classmethod
+    def get(cls):
+        name = input("Name: ")
+        house = input("House: ")
+        return cls(name, house)
+
+    # Getter
+    @property
+    def name(self):
+        return self._name
+
+    # Setter
+    @name.setter
+    def name(self, name):
+        if not name:
+            raise ValueError("Missing name")
+        self._name = name
+
+    # Getter
+    @property
+    def house(self):
+        return self._house
+
+    # Setter
+    @house.setter
+    def house(self, house):
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self._house = house
+
+
+def main():
+    student = Student.get()
+    print(student)
+
+
+if __name__ == "__main__":
+    main()
